3b366ff0fba4c1f210387be4dad9d320a5388e6f
Antonio Elias <aelias12j@gmail.com>
2022-12-30 11:01:32 -0300

    Finalização das configurações iniciais do projeto

--- index.js
+++ index.js
@@ -9,6 +9,56 @@ const port = 3000;
 
 const conn = require('./db/conn');
 
+//template engine
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+//receber resposta do body
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+);
+
+app.use(express.json);
+
+// public path
+app.use(express.static('public'));
+
+// session middleware
+app.use(
+    session({
+        name:'session',
+        secret: 'nosso_secret',
+        resave:false,
+        saveUninitialized:false,
+        store: new FileStore({
+            logFn: function(){},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie:{ 
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly:true, // em produção com o certificado se usaria o https (SSL)
+        },
+    })
+);
+
+
+// flash messages
+app.use(flash());
+
+// set session to res (A ideia aqui é passar os dados do ususario para o front end e se necessario pode ser utilizado o mesmo)
+app.use((req, res, next) => {
+
+    if(req.session.userid){
+        res.locals.session = req.session;
+    }
+
+    next();
+});
+
 conn
     .sync()
     .then(
